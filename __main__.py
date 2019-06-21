bdfb7edeb7c2c499135833d81a526057c4a2222d
BeM835 <turejskijleonid004@gmail.com>
2019-06-21 15:51:22 +1000

    tk widgets changet to ttk

--- __main__.py
+++ __main__.py
@@ -1,9 +1,10 @@
-import tkinter as tk
+import tkinter.ttk as tk
+import tkinter
 import calc
 
-w = tk.Tk()
+w = tkinter.Tk()
 w.title("Calculator")
-w.geometry('110x80')
+w.geometry('110x90')
 
 
 def plus (e):
@@ -27,23 +28,23 @@ numinput.pack()
 numinput1 = tk.Entry(w)
 numinput1.pack()
 
-go_button = tk.Button(w, text="+")
-go_button.place(y=40, x=10)
+go_button = tk.Button(w, text="+", width=2)
+go_button.place(y=40, x=5)
 go_button.bind("<Button-1>", plus)
 
-go_min = tk.Button(w, text="-")
+go_min = tk.Button(w, text="-", width=2)
 go_min.place(y=40, x=35)
 go_min.bind("<Button-1>", minus)
 
-go_mul = tk.Button(w, text="*")
-go_mul.place(y=40, x=60)
+go_mul = tk.Button(w, text="*", width=2)
+go_mul.place(y=40, x=65)
 go_mul.bind("<Button-1>", multiply)
 
-go_div = tk.Button(w, text="/")
-go_div.place(y=40, x=85)
+go_div = tk.Button(w, text="/", width=2)
+go_div.place(y=40, x=95)
 go_div.bind("<Button-1>", divide)
 
 result = tk.Label(w)
-result.place(y=60, x=5, width=100)
+result.place(y=70, x=0, width=110)
 
 w.mainloop()
--- readme.md
+++ readme.md
@@ -1,5 +1,6 @@
-[![Build Status](https://travis-ci.org/BM835/calculator-py.svg?branch=master)](https://travis-ci.org/BM835/calculator-py)
 # calculator on python
+
+[![Build Status](https://travis-ci.org/BM835/calculator-py.svg?branch=master)](https://travis-ci.org/BM835/calculator-py)
 
 just calculator on python
 
