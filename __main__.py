5e66e3c82ac5494176a325bf8595bb4dbbfcd5b5
BeM835 <turejskijleonid004@gmail.com>
2019-06-21 15:40:52 +1000

    all buttons now in a row

--- __main__.py
+++ __main__.py
@@ -3,6 +3,7 @@ import calc
 
 w = tk.Tk()
 w.title("Calculator")
+w.geometry('110x80')
 
 
 def plus (e):
@@ -27,22 +28,22 @@ numinput1 = tk.Entry(w)
 numinput1.pack()
 
 go_button = tk.Button(w, text="+")
-go_button.pack()
+go_button.place(y=40, x=10)
 go_button.bind("<Button-1>", plus)
 
 go_min = tk.Button(w, text="-")
-go_min.pack()
+go_min.place(y=40, x=35)
 go_min.bind("<Button-1>", minus)
 
 go_mul = tk.Button(w, text="*")
-go_mul.pack()
+go_mul.place(y=40, x=60)
 go_mul.bind("<Button-1>", multiply)
 
 go_div = tk.Button(w, text="/")
-go_div.pack()
+go_div.place(y=40, x=85)
 go_div.bind("<Button-1>", divide)
 
 result = tk.Label(w)
-result.pack()
+result.place(y=60, x=5, width=100)
 
 w.mainloop()
