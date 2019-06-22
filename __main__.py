fd9bfda68b0e2cfc8e8423c57a4376a12cadeb7a
BeM835 <turejskijleonid004@gmail.com>
2019-06-22 17:08:20 +1000

    `=` button added

--- __main__.py
+++ __main__.py
@@ -4,7 +4,7 @@ import calc
 
 w = tkinter.Tk()
 w.title("Calculator")
-w.geometry('110x90')
+w.geometry('110x115')
 
 
 def plus (e):
@@ -25,26 +25,30 @@ def divide (e):
 
 numinput = tk.Entry(w)
 numinput.pack()
-numinput1 = tk.Entry(w)
-numinput1.pack()
+
+go_res = tk.Button(w, text="=", width=20)
+go_res.place(y=70, x=0)
+
+numinput1 = tk.Entry(w, width=20)
+numinput1.place(y=50, x=0)
 
 go_button = tk.Button(w, text="+", width=2)
-go_button.place(y=40, x=5)
+go_button.place(y=25, x=5)
 go_button.bind("<Button-1>", plus)
 
 go_min = tk.Button(w, text="-", width=2)
-go_min.place(y=40, x=35)
+go_min.place(y=25, x=35)
 go_min.bind("<Button-1>", minus)
 
 go_mul = tk.Button(w, text="*", width=2)
-go_mul.place(y=40, x=65)
+go_mul.place(y=25, x=65)
 go_mul.bind("<Button-1>", multiply)
 
 go_div = tk.Button(w, text="/", width=2)
-go_div.place(y=40, x=95)
+go_div.place(y=25, x=95)
 go_div.bind("<Button-1>", divide)
 
 result = tkinter.Label(w)
-result.place(y=70, x=0, width=110)
+result.place(y=95, x=0, width=110)
 
 w.mainloop()
